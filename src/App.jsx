13d4c91bce93a0a83707ca1484428145e559c5df
Ashish Mishra <ashish.aashu2004@gmail.com>
2026-03-26 20:51:28 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import 'aframe'
+
+function ARScene() {
+  return (
+    <a-scene
+      xr-mode="ar"
+      renderer="colorManagement: true"
+      embedded
+      style={{ width: '100%', height: '80vh' }}
+    >
+      <a-box
+        id="box"
+        position="0 0 -0.8"
+        color="#4CC3D9"
+        scale="0.3 0.3 0.3"
+      ></a-box>
+
+      <a-entity camera look-controls></a-entity>
+    </a-scene>
+  )
+}
+
+export default function App() {
+  const [clr, setClr] = useState('#4CC3D9')
+
+  const toggle = () => {
+    const newColor = clr === '#4CC3D9' ? '#EF2D5E' : '#4CC3D9'
+    setClr(newColor)
+
+    const box = document.querySelector('#box')
+    if (box) {
+      box.setAttribute('color', newColor)
+    }
+  }
+
+  return (
+    <>
+      <button onClick={toggle}>Toggle color</button>
+      <ARScene />
+    </>
+  )
+}
