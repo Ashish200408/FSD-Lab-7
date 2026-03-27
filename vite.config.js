3b89789f6f13ebe2be91b3684a770c7b5469fe01
Ashish Mishra <ashish.aashu2004@gmail.com>
2026-03-27 19:41:33 +0530

    removed pwa config

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'XR Classroom Viewer',
-        short_name: 'XR App',
-        description: 'WebXR AR/VR Classroom Experience',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/icon.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()]
 })
